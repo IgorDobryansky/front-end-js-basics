bef03430acedb781a4980e0cf695d99b13493b6c
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-06-03 12:48:15 +0300

    first lesson

---
+++ lesson-1/main.js
@@ -0,0 +1,119 @@
+// console.log("Hello world!");
+
+// const someConstVariable = " world";
+// console.log(someConstVariable);
+
+// let someAnotherVariable = "ololo";
+// console.log(someAnotherVariable);
+
+// string
+// number
+// boolean
+// BigInt
+// null
+// undefined
+// Symbol
+// object
+
+// const str = "Hello world"
+// console.log(str);
+// const userName = " Juliya"
+
+// const newStr = str + userName
+// console.log(newStr);
+// "Hello world", 'Hello world', `Hello world`
+// const str = "Juliya's apples";
+// console.log(str);
+// const newStr = `Juliya says: "Juliya's apples are bad...!"`;
+// console.log(newStr);
+// const someValue = 13;
+// const str = `Juliya's apples count are ${someValue}`;
+// console.log(str);
+// console.log("str :>> ", typeof str);
+// const num = 123;
+// console.log("num :>> ", typeof num);
+// console.log("NaN :>> ", typeof NaN);
+
+// // console.log(isNaN(12));
+// console.log(Number.isInteger(12));
+// console.log(isFinite("12.12"));
+
+// const isTrue = true;
+// console.log("isTrue :>> ", typeof isTrue);
+// // > , < , == , ===, != , >= , <=
+// const isBigger = 12 > 9;
+// console.log("isBigger :>> ", isBigger);
+// const isLower = 12 < 9;
+// console.log("isLower :>> ", isLower);
+// const isStrictEqual = 12 === 12;
+// console.log("isStrictEqual :>> ", isStrictEqual);
+// const isEqual = "12" == 12;
+// console.log("isEqual :>> ", isEqual);
+// const isEqual = !!"0" == !!0;
+// console.log("isEqual :>> ", isEqual);
+// const isLowerOrEqual = 12 <= 12;
+// console.log("isLowerOrEqual :>> ", isLowerOrEqual);
+// const isBiggerOrEqual = 12 >= 12;
+// console.log("isBiggerOrEqual :>> ", isBiggerOrEqual);
+
+// const ololo = 0;
+// if (ololo >= 0 && ololo <= 12) {
+//   console.log("here");
+// } else {
+//     console.log("not here");
+// }
+
+// let nullish = null;
+// console.log("nullish :>> ", nullish);
+// let something;
+// console.log("something :>> ", something);
+
+// if (!nullish) {
+//     nullish = {
+//         firstName: "Juliya"
+//     };
+// }
+// console.log("nullish :>> ", nullish);
+
+// const firstName = "Juliya";
+
+// const obj = {
+//   firstName: "Vadim",
+//   lastName: "ololo",
+//   ololo: [],
+//   kekeke: {},
+//   getFirstName() {
+//     console.log("this.firstName", this.lastName);
+//   }
+// };
+
+// obj.getFirstName();
+// console.log( obj);
+// const arr = [1,2,3,4]
+// console.log( arr);
+
+// const arr1 = [
+//     {
+//         fitrstName: "123",
+//         lastName: "321"
+//     },
+//     {
+//         fitrstName: "456",
+//         lastName: "654"
+//     }
+// ]
+
+// const root = document.querySelector("#root");
+// const list = document.createElement("ul");
+// root.append(list);
+// for (let i = 0; i < 10; i++) {
+//   const listItem = document.createElement("li");
+//   list.append(listItem);
+//   listItem.innerText = i;
+// }
+
+// console.log(root);
+// root.innerText = "Juliya & Vadim";
+// root.innerHTML = "<h1>Juliya &amp; Vadim</h1>";
+// root.innerText = "Juliya & Vadim";
+// console.log(root.textContent);
