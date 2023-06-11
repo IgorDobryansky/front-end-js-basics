f89eb71c371d29ea811ec8f4850414600b74e2b0
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-06-11 13:19:09 +0300

    add clock

---
+++ lesson-4_clock/main.js
@@ -0,0 +1,5 @@
+const arrowHour = document.querySelector(".hour-arrow");
+const arrowMinute = document.querySelector(".minute-arrow");
+const arrowSecond = document.querySelector(".second-arrow");
+
+
