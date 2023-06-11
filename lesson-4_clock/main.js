289294abadb1730723afab185748704c0f95a7d9
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-06-11 16:33:55 +0300

    finish clock

--- lesson-4_clock/main.js
+++ lesson-4_clock/main.js
@@ -1,5 +1,37 @@
-const arrowHour = document.querySelector(".hour-arrow");
-const arrowMinute = document.querySelector(".minute-arrow");
-const arrowSecond = document.querySelector(".second-arrow");
+const arrowHour = document.querySelector(".arrow-hour");
+const arrowMinute = document.querySelector(".arrow-minute");
+const arrowSecond = document.querySelector(".arrow-second");
 
+let date;
 
+let clock = false;
+
+function start() {
+  date = new Date();
+
+  const rotateSecondsDegree = date.getSeconds() * 6 - 90;
+  console.log(rotateSecondsDegree);
+  const rotateMinutesDegree = date.getMinutes() * 6 - 90;
+  console.log(rotateMinutesDegree);
+  let rotateHoursDeegree;
+  const hourError = date.getMinutes() / 2;
+  if (date.getHours() <= 12) {
+    rotateHoursDeegree = date.getHours() * 30 - 90 + hourError;
+    console.log(rotateHoursDeegree);
+} else if (date.getHours() > 12) {
+    rotateHoursDeegree = (date.getHours() - 12) * 30 - 90 + hourError;
+    console.log(rotateHoursDeegree);
+  }
+
+  arrowSecond.style.transform = `translate(-50%, -50%) rotate(${rotateSecondsDegree}deg)`;
+  arrowMinute.style.transform = `translate(-50%, -50%) rotate(${rotateMinutesDegree}deg)`;
+  arrowHour.style.transform = `translate(-50%, -50%) rotate(${rotateHoursDeegree}deg)`;
+}
+clock = setInterval(start, 1000);
+
+document.body.addEventListener("click", () => {
+  if (clock) {
+    clearInterval(clock);
+    clock = false
+  } else clock = setInterval(start, 1000);
+});
