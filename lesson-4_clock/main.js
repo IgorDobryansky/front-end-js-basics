fe25b318116320fbbd6783c52c3b2a2b36f8d590
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-06-11 16:44:27 +0300

    add perfomance log

--- lesson-4_clock/main.js
+++ lesson-4_clock/main.js
@@ -7,31 +7,37 @@ let date;
 let clock = false;
 
 function start() {
+  const testDate = Date.now();
+  console.log("start", testDate);
   date = new Date();
 
   const rotateSecondsDegree = date.getSeconds() * 6 - 90;
-  console.log(rotateSecondsDegree);
+  //   console.log(rotateSecondsDegree);
   const rotateMinutesDegree = date.getMinutes() * 6 - 90;
-  console.log(rotateMinutesDegree);
+  //   console.log(rotateMinutesDegree);
   let rotateHoursDeegree;
   const hourError = date.getMinutes() / 2;
   if (date.getHours() <= 12) {
     rotateHoursDeegree = date.getHours() * 30 - 90 + hourError;
-    console.log(rotateHoursDeegree);
-} else if (date.getHours() > 12) {
+    // console.log(rotateHoursDeegree);
+  } else if (date.getHours() > 12) {
     rotateHoursDeegree = (date.getHours() - 12) * 30 - 90 + hourError;
-    console.log(rotateHoursDeegree);
+    // console.log(rotateHoursDeegree);
   }
 
   arrowSecond.style.transform = `translate(-50%, -50%) rotate(${rotateSecondsDegree}deg)`;
   arrowMinute.style.transform = `translate(-50%, -50%) rotate(${rotateMinutesDegree}deg)`;
   arrowHour.style.transform = `translate(-50%, -50%) rotate(${rotateHoursDeegree}deg)`;
+
+  console.log("end", Date.now());
+  console.log("difference ", testDate - Date.now());
 }
+
 clock = setInterval(start, 1000);
 
 document.body.addEventListener("click", () => {
   if (clock) {
     clearInterval(clock);
-    clock = false
+    clock = false;
   } else clock = setInterval(start, 1000);
 });
