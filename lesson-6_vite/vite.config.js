027cc47645acf712920ca4f0eff613945cd7bd05
IgorDobryansky <dobryansky.igor.m@gmail.com>
2023-06-16 21:55:33 +0300

    add some vite example

---
+++ lesson-6_vite/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // main config object
+  root: "src/", // change root path from 'project root' to 'project root/src'
+  build: {
+    // configure build options
+    outDir: "../ololo", // change build folder to 'docs
+    minify: "esbuild" // minify build with 'esbuild
+  },
+  server: {
+    // configure dev server
+    open: true, // open on start
+    port: 3333 // port
+  }
+});
